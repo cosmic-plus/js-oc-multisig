cosmic-plus/js-oc-multisig

commit d452fd6271f61b25f162dc070a84ebe0bdcbb474

Switch network only before signing & sending

src/multisig.js
@@ -3,6 +3,7 @@
  */
 const multisig = exports
 
+const axios = require('axios')
 const Buffer = require('./buffer')
 const shareSignatures = require('./share-signatures')
 const resolve = require('./resolve')
@@ -73,8 +74,8 @@ multisig.pushSignatures = async function (conf, transaction, keypair) {
   }
 
   saveNetwork()
-  const accountId = keypair.publicKey()
-  const pushTx = await shareSignatures.makePushTx(conf, transaction, accountId)
+  await checkAccountExist(conf.multisig, account.id)
+  const pushTx = await shareSignatures.makePushTx(conf, transaction, account.id)
   const horizonResponse = sendOrReturn(conf.multisig, pushTx, keypair)
   horizonResponse.finally(restoreNetwork)
   return horizonResponse
@@ -184,6 +185,19 @@ async function getAccount (conf, user) {
 }
 function isAccountResponse (obj) {
   return obj && obj._baseAccount && obj._baseAccount instanceof StellarSdk.Account
+}
+
+/**
+ * Create `accoundId` if it is empty & on test network.
+ */
+async function checkAccountExist (conf, accountId) {
+  if (await resolve.accountIsEmpty(conf, accountId)) {
+    if (conf.network === 'test') {
+      return axios('https://friendbot.stellar.org/?addr=' + accountId)
+    } else {
+      throw new Error("Account doesn't exist on the requested network: " + conf.network)
+    }
+  }
 }
 
 /**

src/share-signatures.js
@@ -1,7 +1,6 @@
 'use_strict'
 const shareSignatures = exports
 
-const axios = require('axios')
 const messenger = require('./messenger')
 const resolve = require('./resolve')
 
@@ -22,7 +21,6 @@ shareSignatures.makePushTx = async function (conf, transaction, accountId) {
   if (!newSignatures.length) return null
 
   /// Make the transaction that puts signatures on-chain.
-  await checkAccountExist(conf.multisig, accountId)
   const sender = await resolve.account(conf.multisig, accountId)
   const destination = conf.multisig.id
   const object = new StellarSdk.Memo('return', txHash)
@@ -105,17 +103,4 @@ function mergeSignatures (transaction, signatures, txHash, signers) {
 function makeDecorated (signer, signature) {
   const Constructor = StellarSdk.xdr.DecoratedSignature
   return new Constructor({ hint: signer.signatureHint(), signature: signature })
-}
-
-/**
- * Create `accoundId` if it is empty & on test network.
- */
-async function checkAccountExist (conf, accountId) {
-  if (await resolve.accountIsEmpty(conf, accountId)) {
-    if (conf.network === 'test') {
-      return axios('https://friendbot.stellar.org/?addr=' + accountId)
-    } else {
-      throw new Error("Account doesn't exist on the requested network: " + conf.network)
-    }
-  }
 }

src/resolve.js
@@ -2,7 +2,7 @@
  * Contains functions that probe the blockchain or federation servers to collect
  * datas.
  *
- * Note: this is copied from js-cosmic-lib and I didn't cleaned it up yet
+ * Note: this is copied from js-cosmic-lib and I didnt cleaned it up yet
  *
  * @private
  * @exports resolve
@@ -12,32 +12,52 @@ const resolve = exports
 const helpers = require('./helpers')
 
 /**
- * Select the network to be used by `StellarSdk` as being `c` current
- * network.
  *
  */
+resolve.server = function (conf, network = conf.network, server = conf.server) {
+  const passphrase = networkPassphrase(network)
+  if (!passphrase) throw new Error('No network selected.')
+  return getServer(passphrase, server)
+}
+
+/**
+ * Select the network to be used by `StellarSdk` as being `c` current
+ * network.
+ */
 resolve.network = function (conf, network = conf.network, server = conf.server) {
-  const currentNetwork = StellarSdk.Network.current()
-  let passphrase = currentNetwork && currentNetwork.networkPassphrase()
+  let passphrase = networkPassphrase()
+  const newPassphrase = networkPassphrase(network)
 
-  if (network) {
-    let newPassphrase
-    if (network === 'public') newPassphrase = StellarSdk.Networks.PUBLIC
-    else if (network === 'test') newPassphrase = StellarSdk.Networks.TESTNET
-    else newPassphrase = network
-
-    if (passphrase !== newPassphrase) {
-      console.log('Switch to network: ' + network)
-      StellarSdk.Network.use(new StellarSdk.Network(newPassphrase))
-      passphrase = newPassphrase
-    }
+  if (passphrase !== newPassphrase) {
+    console.log('Switch to network: ' + network)
+    StellarSdk.Network.use(new StellarSdk.Network(newPassphrase))
+    passphrase = newPassphrase
   } else if (!passphrase) {
-    throw new Error('No selected network')
+    throw new Error('No network selected.')
   }
 
   return getServer(passphrase, server)
 }
 
+/**
+ * Returns passphrase for `network` or for current network.
+ */
+function networkPassphrase (network) {
+  if (network) {
+    if (network === 'public') return StellarSdk.Networks.PUBLIC
+    else if (network === 'test') return StellarSdk.Networks.TESTNET
+    else return network
+  } else {
+    const currentNetwork = StellarSdk.Network.current()
+    if (currentNetwork) return currentNetwork.networkPassphrase()
+  }
+}
+
+/**
+ * If `url` is given, set it as the default horizon instance for `passphrase`
+ * network and returns the corresponding Server object.
+ * Else, retrieve the default Server object for `passphrase` network.
+ */
 const serverSaves = {}
 const networkDefaultServer = {}
 function getServer (passphrase, url) {
@@ -131,7 +151,7 @@ async function addressResolver (c, address) {
  * @return {Object} The account response
  */
 resolve.account = async function (c, address, network = c.network) {
-  const server = resolve.network(c, network)
+  const server = resolve.server(c, network)
   const account = await resolve.address(c, address)
   const publicKey = account.account_id
   // try {

test/demo.js
@@ -16,8 +16,12 @@ async function test () {
   console.log('=========== Test Network ===========')
   console.log('')
   multisig.useNetwork('test')
+    //await report('multisig.isEnabled(account1)')
+    //await report('multisig.setup(account1, { id: StellarSdk.Keypair.random().publicKey() })')
     //await report('multisig.pushTransaction(transaction1_signed, account1)')
+    //await report('multisig.pushTransaction(transaction2_signed, account1')
     //await report('multisig.listTransactions(account1.publicKey())')
+    //return
 
     await report('multisig.isEnabled(account1)')
     await report('multisig.isEnabled(account2)')

commit a060cb4c19155424d20f3a09e4dd55983aef3864

Fix a regression introduced in d452fd6

src/multisig.js
@@ -74,8 +74,9 @@ multisig.pushSignatures = async function (conf, transaction, keypair) {
   }
 
   saveNetwork()
-  await checkAccountExist(conf.multisig, account.id)
-  const pushTx = await shareSignatures.makePushTx(conf, transaction, account.id)
+  const senderId = keypair.publicKey()
+  await checkAccountExist(conf.multisig, senderId)
+  const pushTx = await shareSignatures.makePushTx(conf, transaction, senderId)
   const horizonResponse = sendOrReturn(conf.multisig, pushTx, keypair)
   horizonResponse.finally(restoreNetwork)
   return horizonResponse

src/share-signatures.js
@@ -9,7 +9,7 @@ const resolve = require('./resolve')
  * `transaction`. The sharing transactions should be signed by `accountId` and
  * sent to `conf.multisig.network`.
  */
-shareSignatures.makePushTx = async function (conf, transaction, accountId) {
+shareSignatures.makePushTx = async function (conf, transaction, senderId) {
   const signatures = transaction.signatures.map(entry => entry.signature())
   if (!transaction.signatures.length) return null
 
@@ -21,7 +21,7 @@ shareSignatures.makePushTx = async function (conf, transaction, accountId) {
   if (!newSignatures.length) return null
 
   /// Make the transaction that puts signatures on-chain.
-  const sender = await resolve.account(conf.multisig, accountId)
+  const sender = await resolve.account(conf.multisig, senderId)
   const destination = conf.multisig.id
   const object = new StellarSdk.Memo('return', txHash)
   const message = Buffer.concat(signatures)